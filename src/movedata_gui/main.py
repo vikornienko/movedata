cb7dfce215b276474800c10f8206bdcd4c1a0f1a
vikornienko <vikornienko76@gmail.com>
2025-12-07 18:09:22 +0500

    Создание стартового окна для проверки работоспособности.

--- src/movedata_gui/main.py
+++ src/movedata_gui/main.py
@@ -1,5 +1,30 @@
-def main():
-    print("Hello from movedata!")
+import sys
+from loguru import logger
+from PySide6.QtWidgets import QApplication
+from movedata_gui.ui.main_window import MainWindow
+
+
+def setup_logging() -> None:
+    logger.remove()
+    logger.add(
+        sys.stderr,
+        format="<green>{time:HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan> - <level>{message}</level>",
+        level="DEBUG",
+        colorize=True
+    )
+
+def main() -> None:
+    setup_logging()
+    logger.info("Start application.")
+
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    exit_code = app.exec()
+    logger.info(f"Exit from the application with code: {exit_code}")
+    sys.exit(exit_code)
 
 
 if __name__ == "__main__":
